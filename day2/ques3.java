ef654ba34b788708e10db68c55945872072f8258
Shubhi Gupta <87714628+shubhigupta2604@users.noreply.github.com>
2021-10-12 21:03:33 +0530

    ques3.java

---
+++ day2/ques3.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class factorial {
+    public static void main(String[] args) {
+        int n=10;
+        int i;
+        int fact=1;
+        for(i=1;i<=10;i++){
+          fact=fact*i;
+        }
+        System.out.println(fact);
+    }
+}
