5527ffd70f97d8cead8991b4e228d6cffb4527c9
Shubhi Gupta <87714628+shubhigupta2604@users.noreply.github.com>
2021-10-12 21:02:48 +0530

    ques2 added

---
+++ day2/ques2.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class natural {
+    public static void main(String[] args) {
+       int i;
+       int n=10;
+       int sum=0;
+       for(i=1;i<=10;i++){
+         sum=sum+i;
+       }
+       System.out.println(sum);
+    }
+}
