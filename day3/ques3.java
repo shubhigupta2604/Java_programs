353154350b11c5c6eaa073dbd52662ee513a0ffb
Shubhi Gupta <87714628+shubhigupta2604@users.noreply.github.com>
2021-10-14 21:33:45 +0530

    ques3 added

---
+++ day3/ques3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Pyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n = sc.nextInt();
+        int i,j;
+        for(i=1;i<=n;i++){
+            for(j=1;j<=i;j++){
+                System.out.print(j);
+
+            }
+
+            System.out.print("\n");
+        }
+
+    }
+}
