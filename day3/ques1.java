f6ac3df29793e1ab079c4be91b3e41ba1556c59e
Shubhi Gupta <87714628+shubhigupta2604@users.noreply.github.com>
2021-10-14 21:42:09 +0530

    ques1 added

--- day3/ques1.java
+++ day3/ques1.java
@@ -1,20 +1,20 @@
 import java.util.Date;
 import java.util.Scanner;
 
-public class reverse {
-    int num=0;
-      int reversenum =0;
-      System.out.println("Enter the number: ");
-      Scanner in = new Scanner(System.in);
-      num = in.nextInt();
-      while( num != 0 )
-      {
-          reversenum = reversenum * 10;
-          reversenum = reversenum + num%10;
-          num = num/10;
-      }
+public class Reverse {
+    public static void main(String[] args) {
+        int num=0;
+        int reversenum =0;
+        System.out.println("Enter the number: ");
+        Scanner in = new Scanner(System.in);
+        num = in.nextInt();
+        while( num != 0 )
+        {
+            reversenum = reversenum * 10;
+            reversenum = reversenum + num%10;
+            num = num/10;
+        }
 
-      System.out.println("Reverse of input number is: "+reversenum);
-   }
+        System.out.println("Reverse of input number is: "+reversenum);
     }
 }
