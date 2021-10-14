3495bf7a2e72e2a70107d9fb1a50b06561105af2
Shubhi Gupta <87714628+shubhigupta2604@users.noreply.github.com>
2021-10-14 21:38:59 +0530

    ques2 added

--- day3/ques2.java
+++ day3/ques2.java
@@ -2,27 +2,29 @@ import java.util.Date;
 import java.util.Scanner;
 
 public class Even_odd {
+    public static void main(String[] args){
     int num=0;
       System.out.println("Enter the number: ");
-      Scanner in = new Scanner(System.in);
-      num = in.nextInt();
-      int count = 0;
+    Scanner in = new Scanner(System.in);
+    num = in.nextInt();
+    int n=num;
+    int count = 0;
 
     while (num != 0) {
-      // num = num/10
-      num /= 10;
-      ++count;
+        // num = num/10
+        num /= 10;
+        ++count;
     }
+    int i,sumEven=0,sumOdd=0;
       for(i=1;i<=count;i++){
-         if (i % 2 == 0)
-                sumEven += n % 10;
-            else
-                sumOdd += n % 10;
-            n /= 10;
-      }
+        if (i % 2 == 0)
+            sumEven += n % 10;
+        else
+            sumOdd += n % 10;
+        n /= 10;
+    }
 
       System.out.println(sumEven);
       System.out.println(sumOdd);
-   }
-    }
 }
+    }
